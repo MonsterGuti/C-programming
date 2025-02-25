992d3cec2c873f8284645c4e44bb7fdd08a8e139
MonsterGuti <gogulanovmartin@gmail.com>
2025-02-25 11:38:04 +0200

    fixing

--- lab_1/odd_or_even.c
+++ lab_1/odd_or_even.c
@@ -7,7 +7,7 @@ int main()
     scanf("%d", &number);
 
     if (number % 2 == 0)
-        printf("%d is odd number.", number);
+        printf("%d is even number.", number);
     else
         printf("%d is odd number.", number);
         return 0;
